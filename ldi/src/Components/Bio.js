fc41356e0f41b38f5ea455e53072397646fd4a01
Allison Fister <allisonfister59@gmail.com>
2020-09-13 15:16:34 -0400

    update bio

--- ldi/src/Components/Bio.js
+++ ldi/src/Components/Bio.js
@@ -4,16 +4,12 @@ import '../Stylesheets/Bio.css';
 function Bio() {
     return (
         <div>
-            <h2> I'm Ashley and I love art
-                <span role="img" aria-label="jsx-a11y/accessible-emoji"> 👩‍🎨</span></h2>
-            <p id="bio"> Inspired by my chigi Lela, Log Dog Ink began Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et ore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in
-                voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum. </p>
+            <h2> I'm Ashley and I love art! <span role="img" aria-label="jsx-a11y/accessible-emoji"> 👩‍🎨</span></h2>
+            <div id="bio"> Inspired by Lela, my beloved Chigi (Chihuahua-Corgi mix), Log Dog Ink began as a small
+                RedBubble shop I used to get my art out into the world. I like experimenting with different mediums
+                and canvasses, watercolor and a digital drawing pad being some of my favorites (but not together of
+                course). I'm always looking to improve my skills as an artist and creative person, so if you have any
+                feedback or suggestions regarding my work, feel free to let me know! </div>
             <p id="contact"> For questions, comments, or commission inquiries, please
                 email me at <a href="mailto:logdogink@gmail.com">logdogink@gmail.com</a>!</p>
         </div>
