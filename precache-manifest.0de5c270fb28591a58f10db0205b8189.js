4c10abe91c13860c1429ab6778df379c7a1469f6
Allison Fister <allisonfister59@gmail.com>
2020-09-14 00:13:25 -0400

    Updates

---
+++ precache-manifest.0de5c270fb28591a58f10db0205b8189.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45bb2ee70e97264fe7fdf3b4716f28b0",
+    "url": "/log-dog-ink/index.html"
+  },
+  {
+    "revision": "b0e08777310db7d3f913",
+    "url": "/log-dog-ink/static/css/main.48e96e81.chunk.css"
+  },
+  {
+    "revision": "3eca7dc149318c537f34",
+    "url": "/log-dog-ink/static/js/2.9d8cf37c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/log-dog-ink/static/js/2.9d8cf37c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e08777310db7d3f913",
+    "url": "/log-dog-ink/static/js/main.5fb15b47.chunk.js"
+  },
+  {
+    "revision": "0a1af4550f7eca9e6b28",
+    "url": "/log-dog-ink/static/js/runtime-main.da0802d9.js"
+  },
+  {
+    "revision": "f37e5cf0df54ee6910e30290e0e79890",
+    "url": "/log-dog-ink/static/media/LelaBellyRub.f37e5cf0.png"
+  },
+  {
+    "revision": "80f64539d080142a9d62f88c076e9860",
+    "url": "/log-dog-ink/static/media/Llama.80f64539.png"
+  },
+  {
+    "revision": "31b6e9a23becf5c8eeae8bfcbfbc6514",
+    "url": "/log-dog-ink/static/media/ShibaInu.31b6e9a2.png"
+  },
+  {
+    "revision": "8d27ee866f8265e1f18da21f37fde363",
+    "url": "/log-dog-ink/static/media/bird-scissors.8d27ee86.jpg"
+  },
+  {
+    "revision": "a3ed4e9ecbb6a07b4e26751012727482",
+    "url": "/log-dog-ink/static/media/catFamily.a3ed4e9e.png"
+  },
+  {
+    "revision": "f85ab6d7dfc4ba2f9e6a8e31679bd969",
+    "url": "/log-dog-ink/static/media/chicago.f85ab6d7.jpg"
+  },
+  {
+    "revision": "be571c29da95c61c1563975ef8488a6f",
+    "url": "/log-dog-ink/static/media/dog.be571c29.jpg"
+  },
+  {
+    "revision": "0076ef20da0034f450650a3f44fdafeb",
+    "url": "/log-dog-ink/static/media/excitedDoggo.0076ef20.JPG"
+  },
+  {
+    "revision": "264ee6e648578155f40f595572103a2e",
+    "url": "/log-dog-ink/static/media/goat.264ee6e6.jpg"
+  },
+  {
+    "revision": "b292b72c4a2a1fba6a5b38cab3922b41",
+    "url": "/log-dog-ink/static/media/man.b292b72c.jpg"
+  },
+  {
+    "revision": "55904e049ca3e050e4d0cdb0cac23abd",
+    "url": "/log-dog-ink/static/media/mountain.55904e04.jpg"
+  },
+  {
+    "revision": "8c8785576d6101efa75eada8831f22be",
+    "url": "/log-dog-ink/static/media/poachers.8c878557.jpg"
+  },
+  {
+    "revision": "6dbebf60362b38022085dbeb24b1fabb",
+    "url": "/log-dog-ink/static/media/tree.6dbebf60.jpg"
+  }
+]);
